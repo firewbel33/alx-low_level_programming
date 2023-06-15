212bd326eaa3d3af9b93fc069412428ec6c77d0a
firewbel33@alx-zero_day.git <freebel33@yahoo.com>
2023-06-15 11:38:55 -0700

    6-size.c

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,8 +1,9 @@
 #include <stdio.h>
 /**
-  *main-c program that prints the size of various types 
-  *return: 0(Success)
-  */
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
 int main(void)
 {
 	printf("Size of a char: %lu\n", sizeof(char));
