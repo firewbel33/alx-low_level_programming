e408af3259f110eee60735e0c319e8d8efed713f
firewbel33@alx-zero_day.git <freebel33@yahoo.com>
2023-06-15 01:12:11 -0700

    4-puts.c

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ * main - a program that prints a line with put fun
+ * return: 0 (Success)
+ */
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
