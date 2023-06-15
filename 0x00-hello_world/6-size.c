828d0e24c61ec4e698503201f101a26d7c1fc5d4
firewbel33@alx-zero_day.git <freebel33@yahoo.com>
2023-06-15 02:18:46 -0700

    6-size.c

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -10,10 +10,10 @@
 	long int c;
 	long long int d;
 	float f;
-printf("Size of the char: %lu byte(s) \n", (unsigned long) sizeof(a));
-printf("Size of the int: %lu byte(s) \n", (unsigned long) sizeof(b));
-printf("Size of the long int: %lu byte(s) \n", (unsigned long) sizeof(c));
-printf("Size of the long long int: %lu byte(s) \n", (unsigned long) sizeof(d));
-printf("Size of the float: %lu byte(s) \n", (unsigned long) sizeof(f));
+printf("Size of the char: %zu byte(s) \n", (unsigned long) sizeof(a));
+printf("Size of the int: %zu byte(s) \n", (unsigned long) sizeof(b));
+printf("Size of the long int: %zu byte(s) \n", (unsigned long) sizeof(c));
+printf("Size of the long long int: %zu byte(s) \n", (unsigned long) sizeof(d));
+printf("Size of the float: %zu byte(s) \n", (unsigned long) sizeof(f));
 return (0);
 }
