8768e2212d63236360b5d6f3aa67efdca20ae21e
firewbel33@alx-zero_day.git <freebel33@yahoo.com>
2023-06-15 11:49:21 -0700

    6-size.c

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -5,11 +5,11 @@
  * Return: Always 0 (Successi)
  */
 int main(void)
-
+{
 	printf("Size of a char: %lu\n", sizeof(char));
 	printf("Size of an int: %lu\n", sizeof(int));
 	printf("Size of a long int: %lu\n", sizeof(long int));
 	printf("Size of a long long int: %lu\n", sizeof(long long int));
-	printf("Size of a float: %lu\n", sizeof(float));
+	printf("Size of a float: %lu", sizeof(float));
 	return (0);
 }
