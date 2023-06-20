4d97c2f5e619d0b0964ddabac001006272531908
firewbel33@alx-zero_day.git <freebel33@yahoo.com>
2023-06-20 00:41:53 -0700

    4-print_alphabt.c

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+
 /**
  * main - Entry point
  * Return: Always 0
@@ -9,9 +10,13 @@ int main(void)
 
 	for (ch = 'a'; ch <= 'z'; ch++)
 	{
-		if ((ch != 'e') && (ch != 'q')			
-			putchar(ch);
+
+	if ((ch != 'e') && (ch != 'q'))
+
+		putchar(ch);
 	}
-	putchar('\n')
+
+	putchar('\n');
+
 	return (0);
 }
