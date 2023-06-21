e3d45ab8dbf77a4e5e94c57606b7a18bbce30984
firewbel33@alx-zero_day.git <freebel33@yahoo.com>
2023-06-21 04:45:17 -0700

    100-times_table.c

---
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -0,0 +1,42 @@
+#include "main.h"
+
+/**
+ * print_times_table - prints the time table
+ *
+ * @n: The value of the time table to be printed
+ *
+ * Return: Always 0
+ */
+void print_times_table(int n)
+{
+	int num, m, p;
+
+	if (n >= 0 && n <= 15)
+	{
+		for (num = 0; num <= n; num++)
+		{
+			_putchar('0');
+			for (m = 1; m <= n; m++)
+			{
+				_putchar(',');
+				_putchar(' ');
+				p = num * m;
+				if (p <= 99)
+					_putchar(' ');
+				if (p <= 9)
+					_putchar(' ');
+				if (p >= 100)
+				{
+					_putchar((p / 100) + '0');
+					_putchar((p / 10) % 10 + '0');
+				}
+				else if (p <= 99 && p >= 10)
+				{
+					_putchar((p / 10) + '0');
+				}
+				_putchar((p % 10) + '0');
+			}
+				_putchar('\n');
+		}
+	}
+}
