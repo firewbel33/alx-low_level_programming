f2023aed6eb68bddd9b94dbcfa85eda41f8362b0
freelung22/printf.git <freebel333@gmail.com>
2023-08-01 05:01:41 -0700

    0-print_listint.c

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -1,4 +1,3 @@
-#include <stdio.h>
 #include "lists.h"
 
 /**
@@ -9,13 +8,14 @@
  */
 size_t print_listint(const listint_t *h)
 {
-	size_t count = 0;
+	size_t num = 0;
 
-	while (h != NULL)
+	while (h)
 	{
 		printf("%d\n", h->n);
-		count++;
+		num++;
 		h = h->next;
 	}
-	return (count);
+
+	return (num);
 }
