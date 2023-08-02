cfaa0be6b97a8f8171e511e73a7a90b8da6da6e9
freelung22/printf.git <freebel333@gmail.com>
2023-08-02 10:41:34 -0700

    10-delete_nodeint.c

---
+++ 0x13-more_singly_linked_lists/10-delete_nodeint.c
@@ -0,0 +1,33 @@
+#include "lists.h"
+
+/**
+ * struct listint_s - singly linked list
+ * @n: integer
+ * @next: points to the next node
+ *
+ * Description: singly linked list node structure
+ * for Holberton project
+ */
+int delete_nodeint_at_index(listint_t **head, unsigned int index)
+{
+	listint_t *curr, *prev;
+	unsigned int i;
+
+	if (head == NULL || *head == NULL)
+		return (-1);
+	curr = *head;
+	prev = NULL;
+	for (i = 0; curr != NULL && i < index; i++)
+	{
+		prev = curr;
+		curr = curr->next;
+	}
+	if (curr == NULL) /* If index is greater than the number of nodes */
+		return (-1);
+	if (prev != NULL) /* If the node to be deleted is not the first node */
+		prev->next = curr->next;
+	else /* If the node to be deleted is the first node */
+		*head = curr->next;
+	free(curr);
+	return (1);
+}
